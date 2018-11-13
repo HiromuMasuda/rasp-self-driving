f3521002a70334deca23d3c8b35af726aa7de4d7
HiromuMasuda <hiromu.masuda.0228@gmail.com>
2018-11-13 19:12:10 +0900

    Add test

---
+++ tests/move_test.py
@@ -0,0 +1,55 @@
+# coding: utf-8
+
+import RPi.GPIO as GPIO
+import time
+import sys
+
+MOTOR_L1 = 26;
+MOTOR_L2 = 19;
+MOTOR_R1 = 13;
+MOTOR_R2 = 6;
+
+GPIO.setmode(GPIO.BCM)
+for io in [MOTOR_A1, MOTOR_A2, MOTOR_B1, MOTOR_B2]:
+    GPIO.setup(io, GPIO.OUT, initial=GPIO.LOW)
+
+def left_cw():
+    GPIO.output(MOTOR_L1, GPIO.HIGH)
+    GPIO.output(MOTOR_L2, GPIO.LOW)
+
+def left_ccw():
+    GPIO.output(MOTOR_L1, GPIO.LOW)
+    GPIO.output(MOTOR_L2, GPIO.HIGH)
+
+def right_cw():
+    GPIO.output(MOTOR_R1, GPIO.HIGH)
+    GPIO.output(MOTOR_R2, GPIO.LOW)
+
+def left_ccw():
+    GPIO.output(MOTOR_R1, GPIO.LOW)
+    GPIO.output(MOTOR_R2, GPIO.HIGH)
+
+direction = sys.argv[1]
+
+if direction == "right":
+    left_cw()
+    right_ccw()
+elif direction == "left":
+    left_ccw()
+    right_cw()
+elif direction == "backward":
+    left_ccw()
+    right_ccw()
+else: # == "forward"
+    left_cw()
+    right_cw()
+
+time.sleep(0.5)
+
+# stop
+GPIO.output(MOTOR_L1, GPIO.LOW)
+GPIO.output(MOTOR_L2, GPIO.LOW)
+GPIO.output(MOTOR_R1, GPIO.LOW)
+GPIO.output(MOTOR_R2, GPIO.LOW)
+
+GPIO.cleanup()

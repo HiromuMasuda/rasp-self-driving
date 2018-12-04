2633439246032e4dc54fe0a869252f0fbf255927
HiromuMasuda <hiromu.masuda.0228@gmail.com>
2018-12-04 16:01:44 +0900

    Add read input function

--- scripts/infrared.js
+++ scripts/infrared.js
@@ -4,4 +4,21 @@ w().ready(function() {
 
   // 1. 毎秒、赤外線モジュールからの入力を確認
   // 2. 動きがあったらslack通知用のマクロを呼び出す
+
+  var INFRARED = 25
+
+  function get_infrared_input() {
+    w().digitalRead(port, function(INFRARED, data){
+      if(data == 1) {
+        console.log("INFRARED: 1");
+        w().callMacro('notifly_slack', []);
+      } else {
+        console.log("INFRARED: 0");
+      }
+    }
+  }
+
+  $("#footer").click(
+    get_infrared_input()
+  );
 });
